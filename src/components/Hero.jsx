1986dc4ce22c4ca0fa5a8b96a69b08e596ddac70
AbdulazizRus <coderdim@gmail.com>
2024-06-05 23:34:23 +0500

    end hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,27 @@ import { logo } from '../assets'
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="logo" className="w-28 object-contain" />
 
-        <button type="button" onClick={() => window.open('https://github.com/sumzai/sumzai')}>GitHub</button>
+        <button
+          className="black_btn"
+          type="button"
+          onClick={() =>
+            window.open('https://github.com/Abdulazizcoderdim/Sumz-AI')
+          }
+        >
+          GitHub
+        </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4o</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
     </header>
   )
 }
